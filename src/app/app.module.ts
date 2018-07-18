d5897d7be7a2065134bc5c0b81039f16509f2ddb
kabildev <kabildev@cgvakindia.com>
2018-07-18 19:45:28 +0530

    About-us files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
+import { RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutUsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about-us', component: AboutUsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
